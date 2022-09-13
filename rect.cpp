eece6c7a0cc7086357c910d6d163d3b63028322b
Ashuutosh2003 <112476165+Ashuutosh2003@users.noreply.github.com>
2022-09-13 11:11:44 +0530

    Add files via upload

---
+++ rect.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+    int l,w;
+    cout<<"enter length: ";
+    cin>>l;
+    cout<<"enter width: ";
+    cin>>w;
+    cout<<"rectangle perimeter: "<<2*l+2*w<<endl;
+
+ cout<<" rect area: "<<l*w;
+    
+    return 0;
+}
---
+++ square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+    int a;
+    cout<<"Enter the first number: ";
+    cin>> a;
+    cout<<"square is "<< a*a;
+    return 0;
+}
